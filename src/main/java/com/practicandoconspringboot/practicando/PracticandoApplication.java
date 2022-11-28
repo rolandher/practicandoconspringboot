8885cb0d3cfb66dad74ae5cea130552e576ff371
Rolando <rolandoantoniohe@ufps.edu.co>
2022-11-28 14:02:11 -0500

    first commit

---
+++ src/main/java/com/practicandoconspringboot/practicando/PracticandoApplication.java
@@ -0,0 +1,14 @@
+package com.practicandoconspringboot.practicando;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PracticandoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PracticandoApplication.class, args);
+		System.out.println("hola mundo");
+	}
+
+}
---
+++ src/test/java/com/practicandoconspringboot/practicando/PracticandoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practicandoconspringboot.practicando;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PracticandoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
